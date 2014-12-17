165c5a195585b3ba8c43bd9e816836ee2a3b80d3
Noah Adams <noah.adams@gmail.com>
2014-12-16 23:01:31 -0800

    properly call async end for lodash

--- vendor/lodash/Gruntfile.js
+++ vendor/lodash/Gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function(grunt) {
     grunt.registerTask('build_dependency', function() {
         var next = this.async();
         grunt.file.copy('repo/dist/lodash.js', 'lodash.js');
+
+        return next();
     });
 
 
